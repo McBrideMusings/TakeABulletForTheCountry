ee354d19b4f6d2dcfb42fbcbe45c872cbfbd17c0
Pierce McBride <piercermcbride@gmail.com>
2016-11-19 20:17:34 -0500

    Froze A-Frame Scene. Wrote Basic About

--- public/js/Generate.js
+++ public/js/Generate.js
@@ -1,16 +1,11 @@
 /*jslint browser:true */
 
 /* Get my HTML References */
-var output = document.querySelector("#out");
-var bulletEntity = document.querySelector("#BulletsHere");
 var incidentsUI = document.querySelector("#incidents");
 var injuriesUI = document.querySelector("#injuries");
 var deathsUI = document.querySelector("#deaths");
 var locationUI = document.querySelector("#location");
-var info = document.querySelector("#info");
 var gun = document.querySelector("#gun");
-var numk = document.querySelector("#numk");
-var numi = document.querySelector("#numi");
 var leftList = document.querySelector("#llist");
 var rightList = document.querySelector("#rlist");
 
@@ -29,10 +24,13 @@ var state = "State";
 var county = "County";
 var intervalID = window.setInterval(Update, 1000);
 var timeElapsed = 0;
-var sumInIncident = 0;
+var timeTo = 0;
 
 function getData(data) {
 	incidentsData = data;
+	timeTo = Math.round(Math.random()*5);
+	if (timeTo <= 2)
+		timeTo += 3;
 }
 
 function Wait() {
@@ -44,7 +42,8 @@ function Wait() {
 }
 
 function Update() {
-	// TODO: add loading screen stuff
+	console.log(timeElapsed);
+	console.log(timeTo);
 	if (!ready) {return;}
 	else {
 		if (timeElapsed === 0) {
@@ -53,10 +52,14 @@ function Update() {
 			injuriesUI.innerHTML = countLocalInjuries + " injures, ";
 			deathsUI.innerHTML = countLocalDeaths + " deaths ";
 		}
-		for (var k = 0; k < bulletIncidents.length; k++) {
-			//playGunSoundFXAni(incidentsToGenerate[0].injured, incidentsToGenerate[0].deaths);
-			generateBullet(bulletIncidents[0]);
-			bulletIncidents.shift();
+		if (bulletIncidents.length > 0) {
+			if (timeElapsed === timeTo) {
+				playGunSoundFXAni();
+				generateBullet(bulletIncidents[0]);
+				bulletIncidents.shift();
+				timeTo = ((Math.random()*5) + 1);
+				timeElapsed = 0;
+			}
 		}
 	}
 	timeElapsed++;
@@ -91,7 +94,8 @@ function generateBullet(incidentParsed) { //myCallback calls this function for e
 		div4.appendChild(document.createTextNode(incidentParsed.Killed + " Killed"));
 		var div5 = document.createElement("div");
 		div5.setAttribute("class", "source");
-		div5.appendChild(document.createTextNode("Source " + incidentParsed.Operations));
+		div5.setAttribute("href", incidentParsed.Operations);
+		div5.appendChild(document.createTextNode("Source"));
 		var img = document.createElement("img");
 		img.setAttribute("src", "assets/bullet-right.png");
 
@@ -126,9 +130,9 @@ function generateBullet(incidentParsed) { //myCallback calls this function for e
 		div4.appendChild(document.createTextNode(incidentParsed.Killed));
 		var div5 = document.createElement("div");
 		div5.setAttribute("class", "source");
-		div5.appendChild(document.createTextNode(incidentParsed.Operations));
+		div5.setAttribute("href", incidentParsed.Operations);	div5.appendChild(document.createTextNode("Source"));
 		var img = document.createElement("img");
-		img.setAttribute("src", "assets/bullet-left.png");
+		img.setAttribute("src", "assets/bullet-right.png");
 
 		div1.appendChild(div2);
 		div1.appendChild(div3);
@@ -146,18 +150,7 @@ function generateBullet(incidentParsed) { //myCallback calls this function for e
 	}
 }
 
-function playGunSoundFXAni(injured, deaths) { //this just uses the emit function in a-frame to make the gun and the cone animate for each incident logged in incidentsToGenerate. Also called in myCallback
-    if (deaths > 0) {
-        //cone.emit('fadeRed');
-        //setTimeout(function(){ cone.emit('fadeWhite'); }, 400);
-    }
-    else if (injured > 0) {
-        //cone.emit('fadeOrange');
-        //setTimeout(function(){ cone.emit('fadeWhite'); }, 400);
-    }
-    else {
-        //console("No Injures or Deaths")
-    }
+function playGunSoundFXAni(injured, deaths) {
     gun.emit('shotsound');
     gun.emit('shot');
 }
